1df2619bd5179be7b084ce96ef9a283185a67a42
pmlpml <pmlpml@163.com>
2018-10-26 17:58:22 +0800

    pefect!

--- web/helloweb/hello.go
+++ web/helloweb/hello.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
+	"strings"
 )
 
 func main() {
 	http.ListenAndServe(":9090", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "hello web!")
+		//which one is the fastest
+		bw := bufio.NewWriter(w)
+		bw.WriteString("hello web!\n")
+		bw.Flush()
+		//which one is the fastest
+		io.Copy(w, strings.NewReader("hello web!\n"))
 	}))
 }
